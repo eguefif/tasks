ba0d42e4eae19693b4c3ab85caae5546b377e43a
Emmanuel Guefif <emmanuel.guefif@demarque.com>
2026-03-26 17:30:06 +0100

    Add basic key store

--- lib/app.ex
+++ lib/app.ex
@@ -2,22 +2,19 @@ defmodule Tasks.App do
   use Application
 
   def start(_type, _args) do
-    IO.puts("Task to do:")
-    tasks = [
-    Tasks.Task.new(1, "going on the moon") |> Tasks.Task.complete(),
-    Tasks.Task.new(3, "Create a rocket") |> Tasks.Task.complete(),
-    Tasks.Task.new(2, "Going back from the moom"),
-    Tasks.Task.new(3, "Take a selfy"),
-    Tasks.Task.new(1, "Build a lab on the moon") |> Tasks.Task.complete(),
-    Tasks.Task.new(2, "Pick up some rocks"),
-    Tasks.Task.new(1, "Tidy up the rocket") |> Tasks.Task.complete(),
-    Tasks.Task.new(3, "Look at earth"),
-    ]
-    |> Enum.filter(fn task -> task.done != true end)
-    |> Enum.sort(fn task1, task2 -> task1.priority < task2.priority end)
-    |> Enum.map(&("   -> " <> &1.name))
-    |> Enum.reduce(&(&2 <> "\n"<> &1))
-    IO.puts(tasks)
+    {:ok, _} = Tasks.Task.start_link(name: :task_store)
+    id = Tasks.Task.add_task(:task_store, 1, "Going to the moon")
+    Tasks.Task.complete(:task_store, id)
+
+    Tasks.Task.add_task(:task_store, 3, "Come back")
+    Tasks.Task.add_task(:task_store, 2, "build a rocket")
+
+    IO.puts("Tasks: ")
+    Tasks.Task.all(:task_store)
+    |> Enum.map(fn task -> "   -> " <> task.name <> " (" <> to_string(task.done) <> ")" end)
+    |> Enum.reduce(fn task, acc -> acc <> "\n" <> task end)
+    |> IO.puts
+
     {:ok, self()}
   end
 end
--- lib/task.ex
+++ lib/task.ex
@@ -1,4 +1,6 @@
 defmodule Tasks.Task do
+  use Agent
+
   @enforce_keys [:priority, :name]
   defstruct priority: 3, name: "", due: true, done: false
 
@@ -10,4 +12,34 @@ defmodule Tasks.Task do
     %Tasks.Task{priority: priority, name: name}
   end
 
+  def start_link(opts) do
+    Agent.start_link(fn -> %{:last_id => 0, :tasks => %{}} end, opts)
+  end
+
+  def get(task_store, id) do
+    Agent.get(task_store, fn data -> Map.get(data.tasks, id) end) 
+  end
+
+  def add_task(bucket, priority, task_name) do
+    last_id = Agent.get(bucket, fn data -> data.last_id end)
+    new_id = last_id + 1
+    Agent.update(bucket, fn data -> 
+    %{:last_id => new_id, :tasks => Map.put(data.tasks, new_id, new(priority, task_name))} end)
+
+    new_id
+  end
+
+  def complete(task_store, id) do
+    Agent.update(task_store, fn data ->
+      %{data | :tasks => Map.update!(data.tasks, id, fn task -> 
+      %{task | done: true}
+      end) }
+    end)
+  end
+  
+  def all(task_store) do
+    tasks = Agent.get(task_store, fn data -> data.tasks end)
+    Map.to_list(tasks)
+    |> Enum.map(fn task -> elem(task, 1) end)
+  end
 end
